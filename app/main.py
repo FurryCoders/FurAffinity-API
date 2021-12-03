88cc3e75cc5df043f09d4ba55a389d8a0117ceb3
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-12-03 23:38:10 +0100

    main - wait Crawl-delay for each request instead of checking for IP

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
+from asyncio import sleep
 from logging import Logger
 from logging import getLogger
-from time import sleep
 from time import time
 from typing import Optional
 
@@ -8,8 +8,8 @@ import faapi
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.exceptions import HTTPException
-from fastapi.responses import ORJSONResponse, RedirectResponse
-from pydantic import BaseModel
+from fastapi.responses import ORJSONResponse
+from fastapi.responses import RedirectResponse
 
 from .__version__ import __version__
 from .exceptions import DisallowedPath
@@ -27,11 +27,6 @@ from .models import serialise_user
 from .models import serialise_user_partial
 from .robots import robots
 
-
-class IPList(BaseModel):
-    ips: dict[str, float] = {}
-
-
 logger: Logger = getLogger("uvicorn")
 
 # noinspection PyUnresolvedReferences
@@ -41,7 +36,6 @@ app: FastAPI = FastAPI(title="Fur Affinity API", version=__version__, openapi_ta
     {"name": "journals", "description": "Get journals"},
     {"name": "users", "description": "Get user information and folders"},
 ])
-ip_list = IPList()
 
 app.add_route("/", lambda r: RedirectResponse("/docs"), ["GET"])
 
@@ -73,83 +67,77 @@ def handle_disallowed_path(_request: Request, _err: faapi.exceptions.ServerError
     return handle_http_exception(_request, DisallowedPath(403))
 
 
-def wait_ip(request: Request):
-    t: float = time()
-    d: Optional[float] = None
-    if (last := ip_list.ips.get(ip := request.client.host, None)) and (d := t - last) < 1:
-        sleep(d)
-    ip_list.ips[ip] = time()
-    logger.info(f"{ip} {t:.07f} {d}")
-
-
 # noinspection GrazieInspection
 @app.post("/submission/{submission_id}/",
           response_model=Submission, response_class=ORJSONResponse, tags=["submissions"])
-def get_submission(request: Request, submission_id: int, body: Body = None):
+async def get_submission(request: Request, submission_id: int, body: Body = None):
     """
     Get a submission object. Public submissions can be queried without cookies.
     """
-    wait_ip(request)
-    return faapi.FAAPI(body.cookies_list() if body else None).get_submission(submission_id)[0]
+    results = (api := faapi.FAAPI(body.cookies_list() if body else None)).get_submission(submission_id)[0]
+    await sleep(api.crawl_delay)
+    return results
 
 
 @app.post("/journal/{journal_id}", response_model=Journal, response_class=ORJSONResponse, tags=["journals"])
-def get_journal(request: Request, journal_id: int, body: Body = None):
+async def get_journal(request: Request, journal_id: int, body: Body = None):
     """
     Get a journal. Public journals can be queried without cookies.
     """
-    wait_ip(request)
-    return faapi.FAAPI(body.cookies_list() if body else None).get_journal(journal_id)
+    results = (api := faapi.FAAPI(body.cookies_list() if body else None)).get_journal(journal_id)
+    await sleep(api.crawl_delay)
+    return results
 
 
 @app.post("/user/{username}/", response_model=User, response_class=ORJSONResponse, tags=["users"])
-def get_user(request: Request, username: str, body: Body = None):
+async def get_user(request: Request, username: str, body: Body = None):
     """
     Get a user's details, profile text, etc. The username may contain underscore (_) characters
     """
-    wait_ip(request)
-    return faapi.FAAPI(body.cookies_list() if body else None).get_user(username.replace("_", ""))
+    results = (api := faapi.FAAPI(body.cookies_list() if body else None)).get_user(username.replace("_", ""))
+    await sleep(api.crawl_delay)
+    return results
 
 
 @app.post("/gallery/{username}/{page}/",
           response_model=SubmissionsFolder, response_class=ORJSONResponse, tags=["users", "submissions"])
-def get_gallery(request: Request, username: str, page: int, body: Body = None):
+async def get_gallery(request: Request, username: str, page: int, body: Body = None):
     """
     Get a list of submissions from the user's gallery folder.
     """
-    wait_ip(request)
-    r, n = faapi.FAAPI(body.cookies_list() if body else None).gallery(username.replace("_", ""), page)
+    r, n = (api := faapi.FAAPI(body.cookies_list() if body else None)).gallery(username.replace("_", ""), page)
+    await sleep(api.crawl_delay)
     return {"results": r, "next": n or None}
 
 
 @app.post("/scraps/{username}/{page}/",
           response_model=SubmissionsFolder, response_class=ORJSONResponse, tags=["users", "submissions"])
-def get_scraps(request: Request, username: str, page: int, body: Body = None):
+async def get_scraps(request: Request, username: str, page: int, body: Body = None):
     """
     Get a list of submissions from the user's scraps folder.
     """
-    wait_ip(request)
-    r, n = faapi.FAAPI(body.cookies_list() if body else None).scraps(username.replace("_", ""), page)
+    r, n = (api := faapi.FAAPI(body.cookies_list() if body else None)).scraps(username.replace("_", ""), page)
+    await sleep(api.crawl_delay)
     return {"results": r, "next": n or None}
 
 
 @app.post("/favorites/{username}/{page}/",
           response_model=SubmissionsFolder, response_class=ORJSONResponse, tags=["users", "submissions"])
-def get_favorites(request: Request, username: str, page: str, body: Body = None):
+async def get_favorites(request: Request, username: str, page: str, body: Body = None):
     """
     Get a list of submissions from the user's favorites folder.
     """
-    wait_ip(request)
-    r, n = faapi.FAAPI(body.cookies_list() if body else None).favorites(username.replace("_", ""), page)
+    r, n = (api := faapi.FAAPI(body.cookies_list() if body else None)).favorites(username.replace("_", ""), page)
+    await sleep(api.crawl_delay)
     return {"results": r, "next": n or None}
 
 
 @app.post("/journals/{username}/{page}/",
           response_model=JournalsFolder, response_class=ORJSONResponse, tags=["users", "journals"])
-def get_scraps(request: Request, username: str, page: int, body: Body = None):
+async def get_scraps(request: Request, username: str, page: int, body: Body = None):
     """
     Get a list of journals from the user's journals folder.
     """
-    wait_ip(request)
-    r, n = faapi.FAAPI(body.cookies_list() if body else None).journals(username.replace("_", ""), page)
+    r, n = (api := faapi.FAAPI(body.cookies_list() if body else None)).journals(username.replace("_", ""), page)
+    await sleep(api.crawl_delay)
     return {"results": r, "next": n or None}
