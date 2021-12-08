171d2fccfb6e12ab03fe85ce211b367e5324920e
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-12-08 15:47:55 +0100

    exceptions - use detail as first argument for errors

--- app/exceptions.py
+++ app/exceptions.py
@@ -5,18 +5,18 @@ from fastapi.exceptions import HTTPException
 
 
 class NotFound(HTTPException):
-    def __init__(self, status_code: int = status.HTTP_404_NOT_FOUND, detail: Any = None,
+    def __init__(self, detail: Any = None, status_code: int = status.HTTP_404_NOT_FOUND,
                  headers: dict[str, Any] = None):
         super(NotFound, self).__init__(status_code, detail, headers)
 
 
 class DisallowedPath(HTTPException):
-    def __init__(self, status_code: int = status.HTTP_401_UNAUTHORIZED, detail: Any = None,
+    def __init__(self, detail: Any = None, status_code: int = status.HTTP_401_UNAUTHORIZED,
                  headers: dict[str, Any] = None):
         super(DisallowedPath, self).__init__(status_code, detail, headers)
 
 
 class Unauthorized(HTTPException):
-    def __init__(self, status_code: int = status.HTTP_403_FORBIDDEN, detail: Any = None,
+    def __init__(self, detail: Any = None, status_code: int = status.HTTP_403_FORBIDDEN,
                  headers: dict[str, Any] = None):
         super(Unauthorized, self).__init__(status_code, detail, headers)
